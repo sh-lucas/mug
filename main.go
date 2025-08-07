8cc5ce5abab22cbbebab7bf9720659d220ce5923
Lucas Schwalm Silva <lnaoedelixo42@gmail.com>
2025-08-07 20:22:45 -0300

    chore: small code organization on main

--- main.go
+++ main.go
@@ -20,15 +20,29 @@ func main() {
 
 // the process is already killed. Must return new process to track.
 func rebuild() *exec.Cmd {
-	// auto generates code
-	generator.GenerateRouter()
-
 	// prepares the statement
 	cmd := exec.Command("go", "run", ".")
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
+	// groups the processes
+	cmd.SysProcAttr = &syscall.SysProcAttr{Setpgid: true}
+
+	// before generateCode() so then it overrides it
 	cmd.Env = os.Environ()
 
+	generateCode(cmd)
+
+	if err := cmd.Start(); err != nil {
+		log.Printf(global.Red+"❌ Failed to start new process: %v", err)
+	}
+
+	return cmd
+}
+
+// logic for generating all the code before executing the command
+func generateCode(cmd *exec.Cmd) {
+	// auto generates code
+	generator.GenerateRouter()
 	// if there is a .env, generate the code for it
 	// and inject in the prepared process
 	envs, err := godotenv.Read(".env")
@@ -39,13 +53,4 @@ func rebuild() *exec.Cmd {
 			cmd.Env = append(cmd.Env, fmt.Sprintf("%s=%s", k, envs[k]))
 		}
 	}
-
-	// groups all processes and the current application
-	cmd.SysProcAttr = &syscall.SysProcAttr{Setpgid: true}
-
-	if err := cmd.Start(); err != nil {
-		log.Printf(global.Red+"❌ Failed to start new process: %v", err)
-	}
-
-	return cmd
 }
