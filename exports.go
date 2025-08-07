f93ea70da9a766141c2f570b810228881c4dfd47
Lucas Schwalm Silva <lnaoedelixo42@gmail.com>
2025-08-07 20:44:56 -0300

    chore: bold colors

--- exports.go
+++ exports.go
@@ -3,19 +3,31 @@ package main
 import "github.com/sh-lucas/mug/global"
 
 var Colors = struct {
-	Reset  string
-	Red    string
-	Green  string
-	Yellow string
-	Blue   string
-	Purple string
-	Cyan   string
+	Reset      string
+	Red        string
+	Green      string
+	Yellow     string
+	Blue       string
+	Purple     string
+	Cyan       string
+	BoldRed    string
+	BoldGreen  string
+	BoldYellow string
+	BoldBlue   string
+	BoldPurple string
+	BoldCyan   string
 }{
-	Reset:  global.Reset,
-	Red:    global.Red,
-	Green:  global.Green,
-	Yellow: global.Yellow,
-	Blue:   global.Blue,
-	Purple: global.Purple,
-	Cyan:   global.Cyan,
+	Reset:      global.Reset,
+	Red:        global.Red,
+	Green:      global.Green,
+	Yellow:     global.Yellow,
+	Blue:       global.Blue,
+	Purple:     global.Purple,
+	Cyan:       global.Cyan,
+	BoldRed:    global.BoldRed,
+	BoldGreen:  global.BoldGreen,
+	BoldYellow: global.BoldYellow,
+	BoldBlue:   global.BoldBlue,
+	BoldPurple: global.BoldPurple,
+	BoldCyan:   global.BoldCyan,
 }
--- global/colors.go
+++ global/colors.go
@@ -1,11 +1,17 @@
 package global
 
 var (
-	Reset  = "\033[0m"
-	Red    = "\033[31m"
-	Green  = "\033[32m"
-	Yellow = "\033[33m"
-	Blue   = "\033[34m"
-	Purple = "\033[35m"
-	Cyan   = "\033[36m"
+	Reset      = "\033[0m"
+	Red        = "\033[31m"
+	Green      = "\033[32m"
+	Yellow     = "\033[33m"
+	Blue       = "\033[34m"
+	Purple     = "\033[35m"
+	Cyan       = "\033[36m"
+	BoldRed    = "\033[1;31m"
+	BoldGreen  = "\033[1;32m"
+	BoldYellow = "\033[1;33m"
+	BoldBlue   = "\033[1;34m"
+	BoldPurple = "\033[1;35m"
+	BoldCyan   = "\033[1;36m"
 )
